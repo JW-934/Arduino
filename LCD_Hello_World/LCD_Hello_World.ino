426c333e2d29aade7fc9f0d504f1658cfcdd05d3
Jesse <jessew7980@gmail.com>
2021-12-04 01:29:25 -0800

    Update LCD_Hello_World.ino

--- LCD_Hello_World/LCD_Hello_World.ino
+++ LCD_Hello_World/LCD_Hello_World.ino
@@ -9,6 +9,8 @@ void setup()
   LiquidCrystal lcd(7, 8, 9, 10, 11, 12); // initialize LCD by outlining setting pins
   lcd.begin(16, 2); // 2 rows, 16 columns
   lcd.print("Hello world!");
+  lcd.setCursor(0,1);
+  lcd.print("-Jesse");
 }
 
 void loop() 
